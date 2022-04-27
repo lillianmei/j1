af9bc8c44d0014a45e12e0f79a2e4d7bdc1dc875
lilian mei <jhongmei@a-sir.com.tw>
2022-04-27 18:31:32 +0800

    導連頁獨立版本

--- js/index.js
+++ js/index.js
@@ -1,48 +1,36 @@
 $(function () {
-
   //啟動滑動效果
-  AOS.init({
-    mirror:true,
-    // easing: 'ease-out-back',
-  });
-    // $.scrollify({
-    //   section: ".panel",
-    //   scrollSpeed: 2000,
-    //   sectionName: false,
-    //   updateHash: true,
-    //   easing: "easeOutCubic",
-    //   setHeights: false,
-    // });
+
+  // $.scrollify({
+  //     section: ".panel",
+  //     scrollSpeed: 2000,
+  //     sectionName: false,
+  //     updateHash: true,
+  //     easing: "easeOutCubic",
+  //     setHeights: false,
+  //     after: function (e) {
+  //       console.log(e)
+  //       if(e===1){
+  //         $.scrollify.destroy()
+  //       }
+  //     },
+  //   });
 
   //NAV效果
   $(window).on('scroll resize', function (e) {
-    let scrollHeight = 600
-    let sectionY = $('section[data-id]').offset().top;
-      $nav = $('#nav');
+    let scrollHeight = 300
+    $nav = $('#nav');
+    $landing = $('#landing')
     if ($(window).scrollTop() > scrollHeight) {
       // $nav.addClass('mini');
-      $nav.css('z-index','2')
-      $nav.css('opacity', '1')
-      $('html,body').animate({
-        scrollTop: div
-      }, 500);
+      // $nav.css('z-index','2')
+      // $nav.css('opacity', '1')
     } else {
-      $nav.css('z-index', '0')
-      $nav.css('opacity', '0')
+      // $nav.css('z-index', '0')
+      // $nav.css('opacity', '0')
       // $nav.removeClass('mini');
     }
   })
-  //scroll滑動效果
-   $(window).on('scroll resize', function (e) {
-     let sectionY = $('section[data-id]').offset().top;
-     let scrollHeight = sectionY/2
-
-     if ($(window).scrollTop() > scrollHeight) {
-       $('html,body').animate({
-         scrollTop: sectionY
-       }, 500);
-     }
-   })
   //account hover效果
   const account = document.querySelector('.user-account')
   account.addEventListener('mouseenter', e => {
@@ -53,7 +41,5 @@ $(function () {
     account.classList.remove('-active')
     account.classList.add('-leave')
   });
-
-  //
 
 });
