abe079eac59fb93219a053842a8bf062ade6c6b1
lilian mei <jhongmei@a-sir.com.tw>
2022-05-17 10:05:33 +0800

    效果 樣式調整

--- js/index.js
+++ js/index.js
@@ -1,49 +1,29 @@
 $(function () {
   const landing = document.querySelector('#landing')
-  //啟動滑動效果
-  // $.scrollify({
-  //     section: ".panel",
-  //     scrollSpeed: 2000,
-  //     sectionName: false,
-  //     updateHash: true,
-  //     easing: "easeOutCubic",
-  //     setHeights: false,
-  //     after: function (e) {
-  //       console.log(e)
-  //       if(e===1){
-  //         $.scrollify.destroy()
-  //       }
-  //     },
-  //   });
-  if(landing){
-     $(window).on('mousewheel', function (e) {
-       if (e) {
-         $('body').addClass('start-scroll')
-         $('body').removeClass('no-scroll')
-       }
-     });
-     $('#landing').on('click', function () {
-       $('body').addClass('start-scroll')
-       $('body').removeClass('no-scroll')
-     });
-     
-  }else{
-     $('body').removeClass('no-scroll')
+  if (landing) {
+    $(window).on('mousewheel', function (e) {
+      if (e) {
+        $('body').addClass('start-scroll')
+        setTimeout(function () {
+          $('body').removeClass('no-scroll')
+          $('#landing h1').css('display', 'none')
+          $('#landing .welcome').css('display', 'none')
+
+        }, 800);
+      }
+    });
+  } else {
+    $('body').removeClass('no-scroll')
   }
-  
+
 
   //NAV效果
   $(window).on('scroll resize', function (e) {
     let scrollHeight = 300
     $nav = $('#nav');
-    $landing = $('#landing')
     if ($(window).scrollTop() > scrollHeight) {
       // $nav.addClass('mini');
-      // $nav.css('z-index','2')
-      // $nav.css('opacity', '1')
     } else {
-      // $nav.css('z-index', '0')
-      // $nav.css('opacity', '0')
       // $nav.removeClass('mini');
     }
   })
